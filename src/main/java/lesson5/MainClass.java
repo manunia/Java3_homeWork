1251e5af44615eee558e6cc0504738b8eb93000b
manunia <timoshenkomaria@mail.ru>
2018-08-24 17:42:31 +0300

    сделана проверка кто победитель

--- src/main/java/lesson5/MainClass.java
+++ src/main/java/lesson5/MainClass.java
@@ -1,7 +1,6 @@
 package lesson5;
 
 import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.Semaphore;
 
 public class MainClass {
     public static final int CARS_COUNT = 4;
@@ -28,11 +27,11 @@ public class MainClass {
         for (int i = 0; i < cars.length; i++) {
             cars[i] = new Car(race,20 + (int) (Math.random() * 10));
         }
-
+        Thread[] threads = new Thread[cars.length];
         for (int i = 0; i < cars.length; i++) {
-            new Thread(cars[i]).start();
+            threads[i] = new Thread(cars[i]);
+            threads[i].start();
         }
-
         try {
             cdl2.await();
         } catch (InterruptedException e) {
@@ -43,6 +42,13 @@ public class MainClass {
             cdl.await();
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+        //узнаем кто победитель
+        for (int i = 0; i < cars.length; i++) {
+            if (threads[i].getState() == Thread.State.TERMINATED) {
+                System.out.println(cars[i].getName() + " - WINNER!!!");
+                break;
+            }
         }
 
         System.out.println( "ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!" );
